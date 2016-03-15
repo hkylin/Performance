f08db3aad866501f02f1fd4eee91217702ced006
yours2008 <yours2008@126.com>
2016-03-15 21:01:41 +0800

    1、首页 增加  合作者显示 2、尝试解决devise zh-CN问题

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -15,7 +15,7 @@ class Plan < ActiveRecord::Base
   
   # validates_presence_of :department
   validates_presence_of :start_date, :end_date, :name, :number, :plan_type, :rate   , :message => "不能为空" # 最少 2 #, :parter
-  validates_length_of :name, :minimum => 2 ,:message => "名称最少4个字节" 
+  validates_length_of :name, :minimum => 4 ,:message => "名称最少4个字节" 
   validates_numericality_of :scale, :greater_than => 30000000, :message => "最小规模3000万" # 最少 2 
   validates_uniqueness_of :number,  :on => :create, :message => "计划编号不唯一" 
   validates_uniqueness_of :name , :on => :create,:message => "计划名称不唯一" 
