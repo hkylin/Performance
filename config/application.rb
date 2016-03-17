a2bc4eee77cd74ce571f47554b20f956a9fa9934
yours2008 <yours2008@126.com>
2016-03-17 11:28:24 +0800

    修改中文配置属性

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Performance
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'zh-CN', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"zh-CN"
 
 
