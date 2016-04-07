62b9b05ffac907fef0f26cb5f716d469bf7e2c16
yours2008 <yours2008@126.com>
2016-04-07 09:43:09 +0800

    明确  三个角色，部门成员 staff标识

--- app/models/department_user.rb
+++ app/models/department_user.rb
@@ -1,9 +1,10 @@
 class DepartmentUser < ActiveRecord::Base
   belongs_to :department
   belongs_to :user
-
+  ROLE_TYPE = %w(have admin staff)  
+  validates_inclusion_of :role, in: ROLE_TYPE
   #role 
   	#have   不是直属管理树枝、树叶的关系
   	#admin  部门主管
-  	#       空代表部门成员
+  	#staff  代表部门成员
 end
