8aa45a00febd7d70bef4fc57c4bfe47ebd8a0053
yours2008 <yours2008@126.com>
2016-04-07 09:41:48 +0800

    删除无用的parent字段

--- db/migrate/20160223084934_create_departments.rb
+++ db/migrate/20160223084934_create_departments.rb
@@ -3,8 +3,6 @@ class CreateDepartments < ActiveRecord::Migration
     create_table :departments do |t|
       t.references :sup_department, index: true, foreign_key: true
       t.string :name
-      t.boolean :parent
-
       t.timestamps null: false
     end
   end
