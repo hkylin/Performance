bf74d33a9ca792b8f9b373297a21eea80c8049fa
yours2008 <yours2008@126.com>
2016-04-26 23:09:25 +0800

    设置默认值

--- db/migrate/20160426122235_add_calc_to_departments.rb
+++ db/migrate/20160426122235_add_calc_to_departments.rb
@@ -1,18 +1,18 @@
 class AddCalcToDepartments < ActiveRecord::Migration
   def change
-  	add_column :departments, :income_current, :decimal
-	add_column :departments, :income_year, :decimal
-	add_column :departments, :income_q1, :decimal
-	add_column :departments, :income_q2, :decimal
-	add_column :departments, :income_q3, :decimal
-	add_column :departments, :income_q4, :decimal
-	add_column :departments, :scale_current, :decimal
-	add_column :departments, :channel_income_current, :decimal
-	add_column :departments, :channel_income_year, :decimal
-	add_column :departments, :channel_income_q1, :decimal
-	add_column :departments, :channel_income_q2, :decimal
-	add_column :departments, :channel_income_q3, :decimal
-	add_column :departments, :channel_income_q4, :decimal
-	add_column :departments, :channel_scale_current, :decimal
+  	add_column :departments, :income_current, :decimal, default: 0.0
+	add_column :departments, :income_year, :decimal, default: 0.0
+	add_column :departments, :income_q1, :decimal, default: 0.0
+	add_column :departments, :income_q2, :decimal, default: 0.0
+	add_column :departments, :income_q3, :decimal, default: 0.0
+	add_column :departments, :income_q4, :decimal, default: 0.0
+	add_column :departments, :scale_current, :decimal, default: 0.0
+	add_column :departments, :channel_income_current, :decimal, default: 0.0
+	add_column :departments, :channel_income_year, :decimal, default: 0.0
+	add_column :departments, :channel_income_q1, :decimal, default: 0.0
+	add_column :departments, :channel_income_q2, :decimal, default: 0.0
+	add_column :departments, :channel_income_q3, :decimal, default: 0.0
+	add_column :departments, :channel_income_q4, :decimal, default: 0.0
+	add_column :departments, :channel_scale_current, :decimal, default: 0.0
   end
 end
--- db/migrate/20160426125017_add_calc_to_plans.rb
+++ db/migrate/20160426125017_add_calc_to_plans.rb
@@ -1,8 +1,8 @@
 class AddCalcToPlans < ActiveRecord::Migration
   def change
-  	add_column :plans, :m_whole_fee, :decimal
-	add_column :plans, :m_year_fee, :decimal
-    add_column :plans, :m_scale, :decimal   
-    add_column :plans, :mm_scale, :decimal
+  	add_column :plans, :m_whole_fee, :decimal, default: 0.0
+	add_column :plans, :m_year_fee, :decimal, default: 0.0
+    add_column :plans, :m_scale, :decimal, default: 0.0
+    add_column :plans, :mm_scale, :decimal, default: 0.0
   end
 end
